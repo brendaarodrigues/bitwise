d7deaeba99a2f81d6fc1760eebfecaf8844c5f35
Brenda <brendaa.rodrigues@outlook.com>
2023-10-21 00:05:38 -0300

    feat: creating menu auto hover options

--- src/app/modules/home/menu/menu.component.ts
+++ src/app/modules/home/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { navigationItems } from 'src/app/core/utils/navigation-items';
 
 @Component({
@@ -6,12 +6,30 @@ import { navigationItems } from 'src/app/core/utils/navigation-items';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
   items = navigationItems;
+  currentOpenItem: any = null;
+  currentOpenMenuTrigger: any = null;
 
-  ngOnInit() {}
+  triggerMenuIfNotActive(item: any, menuTrigger: any) {
+    if (this.currentOpenItem && this.currentOpenItem !== item) {
+      this.closeMenu(this.currentOpenItem, this.currentOpenMenuTrigger);
+    }
 
-  toggleDropdown(item: any) {
-    item.showDropdown = !item.showDropdown;
+    if (!item.triggered) {
+      item.triggered = true;
+      menuTrigger.openMenu();
+      this.currentOpenItem = item;
+      this.currentOpenMenuTrigger = menuTrigger;
+    }
+  }
+
+  closeMenu(item: any, menuTrigger: any) {
+    if (item.triggered) {
+      item.triggered = false;
+      menuTrigger.closeMenu();
+      this.currentOpenItem = null;
+      this.currentOpenMenuTrigger = null;
+    }
   }
 }
